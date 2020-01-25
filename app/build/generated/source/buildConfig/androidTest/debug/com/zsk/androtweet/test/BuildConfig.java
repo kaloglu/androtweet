d64890b2e4dbf784b680d5e8027104138f45b46a
kaloglu <cihan@kaloglu.com>
2020-01-25 16:40:34 +0300

    Upgrade version code and name

--- app/build/generated/source/buildConfig/androidTest/debug/com/zsk/androtweet/test/BuildConfig.java
+++ app/build/generated/source/buildConfig/androidTest/debug/com/zsk/androtweet/test/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.zsk.androtweet.test";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 12;
-  public static final String VERSION_NAME = "2.60";
+  public static final int VERSION_CODE = 20;
+  public static final String VERSION_NAME = "3.0.0";
 }
